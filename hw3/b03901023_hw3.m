7f878ee9b294fb188613fc02f5f24e198d40b3a5
AdrianHsu <adrianhsu1995@gmail.com>
2017-05-13 18:05:04 +0800

    done

--- hw3/b03901023_hw3.m
+++ hw3/b03901023_hw3.m
@@ -1,8 +1,11 @@
 clc;
 clear;
 
-score=[1,1,5,5,6,6,5]; %1:Do,2:Re,3:Mi,.....
-beat=[1, 1, 1, 1, 1, 1, 2]; % tempo
 
+% score=[1,1,5,5,6,6,5]; %1:Do,2:Re,3:Mi,.....
+% beat=[1, 1, 1, 1, 1, 1, 2]; % tempo
+score=[1,1,5,5,6,6,5, 4, 4, 3, 3, 2, 2, 1]; %1:Do,2:Re,3:Mi,.....
+beat=[1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2]; % tempo
 name = 'twinkle';
+
 getmusic(score, beat, name); 
